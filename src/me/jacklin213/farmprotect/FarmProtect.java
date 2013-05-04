3b575bd4edb0e472175c946711cc899a824eb6ad
jack lin <jacklin213@gmail.com>
2013-05-05 11:02:03 +1200

    Updated to v1.4

--- src/me/jacklin213/farmprotect/FarmProtect.java
+++ src/me/jacklin213/farmprotect/FarmProtect.java
@@ -1,6 +1,9 @@
 package me.jacklin213.farmprotect;
 
+import java.io.File;
 import java.util.logging.Logger;
+
+import me.jacklin213.farmprotect.ulits.UpdateChecker;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -8,20 +11,39 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FarmProtect extends JavaPlugin implements Listener{
+	
+	public static FarmProtect plugin;
 
 	PluginDescriptionFile pdfFile;
-	private static final Logger log = Logger.getLogger("Minecraft");
-
+	public Logger log = Logger.getLogger("Minecraft");
+	public UpdateChecker updateChecker;
+		
 	public void onDisable() {
 		log.info(String.format("[%s] Disabled Version %s", getDescription()
 				.getName(), getDescription().getVersion()));
 	}
 
 	public void onEnable() {
-		log.info(String.format("[%s] Enabled Version %s by jacklin213", getDescription()
+		
+		Boolean updateCheck = getConfig().getBoolean("updatecheck");
+		
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+		
+		this.updateChecker = new UpdateChecker(this, "http://dev.bukkit.org/server-mods/farmprotect/files.rss");
+		
+		if (updateCheck){
+			if (this.updateChecker.updateNeeded()) {
+				this.log.info(String.format("[%s] A new update is avalible, Version: %s", new Object[] { getDescription().getName(), this.updateChecker.getVersion() }));
+			    this.log.info(String.format("[%s] Get it now from: %s", new Object[] { getDescription().getName(), this.updateChecker.getLink() }));
+			}
+		}
+	    
+		this.log.info(String.format("[%s] Enabled Version %s by jacklin213", getDescription()
 				.getName(), getDescription().getVersion()));
 
 	}
@@ -30,6 +52,20 @@ public class FarmProtect extends JavaPlugin implements Listener{
 	public void noFarmlanddestroy(PlayerInteractEvent event){
 		if(event.getAction() == Action.PHYSICAL && event.getClickedBlock().getType() == Material.SOIL)
 			event.setCancelled(true);
+	}
+	
+	public void createconfig() {
+		// Creates config.yml
+		File file = new File(getDataFolder() + File.separator + "config.yml");
+		// If config.yml doesnt exit
+		if (!file.exists()) {
+			// Tells console its creating a config.yml
+			this.getLogger().info(String.format("[%s] Cannot find config.yml, Generating now....", getDescription().getName()));
+			this.getLogger().info(String.format("[%s] Config generated !", getDescription().getName()));
+			this.getConfig().options().copyDefaults(true);
+			this.saveDefaultConfig();
+		}
+
 	}
 
 }
