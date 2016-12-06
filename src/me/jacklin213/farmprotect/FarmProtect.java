b4e70012bd074170bdc86b9dcb2b0c5ff9d5633e
Jack Lin <jacklin213@gmail.com>
2016-12-07 10:02:07 +1300

    Add MultiWorld support, Add cmd add, remove, toggle

--- src/me/jacklin213/farmprotect/FarmProtect.java
+++ src/me/jacklin213/farmprotect/FarmProtect.java
@@ -1,12 +1,16 @@
 package me.jacklin213.farmprotect;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import me.jacklin213.farmprotect.utils.Updater;
 import me.jacklin213.farmprotect.utils.Updater.UpdateResult;
 import me.jacklin213.farmprotect.utils.Updater.UpdateType;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -26,10 +30,16 @@ public class FarmProtect extends JavaPlugin implements Listener {
 
 	public Logger log;
 	public Updater updater;
-		
+	public String chatPluginName = ChatColor.translateAlternateColorCodes('&', "&c[&6FarmProtect&c] ");
+	List<String> worlds = new ArrayList<>();
+	
 	public void onEnable() {
 		this.setLogger();
 		this.createConfig();
+		
+		this.getConfig().getStringList("Worlds").stream()
+				.filter(world -> Bukkit.getWorld(world) != null)
+				.forEach(world -> worlds.add(world));
 		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(this, this);
@@ -42,32 +52,123 @@ public class FarmProtect extends JavaPlugin implements Listener {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[]) {
-		if (commandLabel.equalsIgnoreCase("farmprotect")) {
-			if (sender.hasPermission("farmprotect.reload")) {
-				if (args[0].equalsIgnoreCase("reload")) {
+		if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("help"))) {
+			sendHelp(sender);
+		} else if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("reload")) {
+				if (sender.hasPermission("farmprotect.reload")) {
 					this.reloadConfig();
-					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c[&6FarmProtect&c]&a Has been reloaded"));
+					sender.sendMessage(chatPluginName + ChatColor.GREEN + "plugin has successfully reloaded.");
+					return true;
+				} else {
+					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
+					return true;
+				}
+			} else if (args[0].equalsIgnoreCase("toggle")) {
+				if (sender.hasPermission("farmprotect.multiworld.toggle")) {
+					if (this.getConfig().getBoolean("MultiWorld")) {
+						this.getConfig().set("MultiWorld", false);
+						sender.sendMessage(chatPluginName + ChatColor.RED + "MultiWorld has been turned off.");
+					} else {
+						this.getConfig().set("MultiWorld", true);
+						sender.sendMessage(chatPluginName + ChatColor.GREEN + "MultiWorld has been turned on.");
+					}
+					try {
+						this.getConfig().save(new File(getDataFolder(), "config.yml"));
+					} catch (IOException e) {
+						e.printStackTrace();
+					} 
+				} else {
+					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
 					return true;
 				}
 			} else {
-				sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
-				return true;
+				sendHelp(sender);
 			}
-		}
+		} else if (args.length > 1) {
+			if (args[0].equalsIgnoreCase("add")) {
+				if (sender.hasPermission("farmprotect.addworld")) {
+					if (Bukkit.getWorld(args[1]) == null) {
+						sender.sendMessage(chatPluginName + ChatColor.RED + "not a valid world.");
+					} else {
+						if (worlds.contains(args[1].toLowerCase())) {
+							sender.sendMessage(chatPluginName + ChatColor.RED + "This world is already protected!");
+						} else {
+							worlds.add(args[1]);
+							this.getConfig().set("Worlds", worlds);
+							try {
+								this.getConfig().save(new File(getDataFolder(), "config.yml"));
+							} catch (IOException e) {
+								e.printStackTrace();
+							} 
+							sender.sendMessage(chatPluginName + ChatColor.GOLD + args[1] + ChatColor.GREEN + "has been added.");
+						}
+					}
+				} else {
+					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
+					return true;
+				}
+			} else if (args[0].equalsIgnoreCase("remove")) {
+				if (sender.hasPermission("farmprotect.removeworld")) {
+					if (!worlds.contains(args[1].toLowerCase())) {
+						sender.sendMessage(chatPluginName + ChatColor.RED + "This world isn't being protected!");
+					} else {
+						worlds.remove(args[1]);
+						this.getConfig().set("Worlds", worlds);
+						try {
+							this.getConfig().save(new File(getDataFolder(), "config.yml"));
+						} catch (IOException e) {
+							e.printStackTrace();
+						} 
+						sender.sendMessage(chatPluginName + ChatColor.YELLOW + args[1] + ChatColor.GREEN + " has been removed.");
+					}
+				} else {
+					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
+					return true;
+				}
+			} else {
+				sendHelp(sender);
+			}
+		} 
 		return false;
+	}
+	
+	public void sendHelp(CommandSender sender) {
+		sender.sendMessage(formatColor("&6o0=======&c[&eFarmProtect&c]&6========0o"));
+		sender.sendMessage(formatColor("&b/NOTE &f: &e" + "All farmprotect commands have an alias of /fp"));
+		sender.sendMessage(formatColor("&b/fp toggle &f- &e" + "Turns on multiworld protection support"));
+    	sender.sendMessage(formatColor("&b/fp add <world> &f- &e" + "Adds a world into protection"));
+    	sender.sendMessage(formatColor("&b/fp remove <world> &f- &e" + "Removes protection for a world"));
+    	sender.sendMessage(formatColor("&b/fp reload &f- &e" + "Reloads the config"));
+	}
+	
+	public String formatColor(String message) {
+		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 	
 	@EventHandler
 	public void soilChangePlayer(PlayerInteractEvent event) {
-		if ((event.getAction() == Action.PHYSICAL) && (event.getClickedBlock().getType() == Material.SOIL)){
-			event.setCancelled(true);
+		if ((event.getAction() == Action.PHYSICAL) && (event.getClickedBlock().getType() == Material.SOIL)) {
+			if (this.getConfig().getBoolean("MultiWorld")) {
+				if (worlds.contains(event.getClickedBlock().getWorld().getName().toLowerCase())) {
+					event.setCancelled(true);
+				}
+			} else {
+				event.setCancelled(true);
+			}
 		}
 	}
 	
 	@EventHandler
 	public void soilChangeEntity(EntityInteractEvent event)	{
-		if ((event.getEntityType() != EntityType.PLAYER) && (event.getBlock().getType() == Material.SOIL)){
-			event.setCancelled(true);
+		if ((event.getEntityType() != EntityType.PLAYER) && (event.getBlock().getType() == Material.SOIL)) {
+			if (this.getConfig().getBoolean("MultiWorld")) {
+				if (worlds.contains(event.getBlock().getWorld().getName().toLowerCase())) {
+					event.setCancelled(true);
+				}
+			} else {
+				event.setCancelled(true);
+			}
 		}
 	}
 	
