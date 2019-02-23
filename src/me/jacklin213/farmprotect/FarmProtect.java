b1f446df5a2f807f84c4bf907347d471644352b7
Jack Lin <jacklin213@gmail.com>
2019-02-23 14:22:58 +1100

    Add ExplosionProtect commands, config

--- src/me/jacklin213/farmprotect/FarmProtect.java
+++ src/me/jacklin213/farmprotect/FarmProtect.java
@@ -3,26 +3,31 @@ package me.jacklin213.farmprotect;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
-
-import me.jacklin213.farmprotect.utils.Updater;
-import me.jacklin213.farmprotect.utils.Updater.UpdateResult;
-import me.jacklin213.farmprotect.utils.Updater.UpdateType;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.jacklin213.farmprotect.utils.Updater;
+import me.jacklin213.farmprotect.utils.Updater.UpdateResult;
+import me.jacklin213.farmprotect.utils.Updater.UpdateType;
 
 public class FarmProtect extends JavaPlugin implements Listener {
 	
@@ -32,6 +37,13 @@ public class FarmProtect extends JavaPlugin implements Listener {
 	public Updater updater;
 	public String chatPluginName = ChatColor.translateAlternateColorCodes('&', "&c[&6FarmProtect&c] ");
 	List<String> worlds = new ArrayList<>();
+	List<Material> cropsList = new ArrayList<>(Arrays.asList(
+	        Material.CROPS,
+	        Material.MELON_STEM,
+	        Material.PUMPKIN_STEM,
+	        Material.CARROT,
+	        Material.POTATO
+	));
 	
 	public void onEnable() {
 		this.setLogger();
@@ -53,37 +65,21 @@ public class FarmProtect extends JavaPlugin implements Listener {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[]) {
 		if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("help"))) {
-			sendHelp(sender);
+			sendHelp(sender, 1);
 		} else if (args.length == 1) {
 			if (args[0].equalsIgnoreCase("reload")) {
 				if (sender.hasPermission("farmprotect.reload")) {
 					this.reloadConfig();
-					sender.sendMessage(chatPluginName + ChatColor.GREEN + "plugin has successfully reloaded.");
+					sender.sendMessage(chatPluginName + ChatColor.GREEN + "Plugin has successfully reloaded.");
 					return true;
 				} else {
 					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
 					return true;
 				}
 			} else if (args[0].equalsIgnoreCase("toggle")) {
-				if (sender.hasPermission("farmprotect.multiworld.toggle")) {
-					if (this.getConfig().getBoolean("MultiWorld")) {
-						this.getConfig().set("MultiWorld", false);
-						sender.sendMessage(chatPluginName + ChatColor.RED + "MultiWorld has been turned off.");
-					} else {
-						this.getConfig().set("MultiWorld", true);
-						sender.sendMessage(chatPluginName + ChatColor.GREEN + "MultiWorld has been turned on.");
-					}
-					try {
-						this.getConfig().save(new File(getDataFolder(), "config.yml"));
-					} catch (IOException e) {
-						e.printStackTrace();
-					} 
-				} else {
-					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
-					return true;
-				}
+			    sendHelp(sender, 2);
 			} else {
-				sendHelp(sender);
+				sendHelp(sender, 1);
 			}
 		} else if (args.length > 1) {
 			if (args[0].equalsIgnoreCase("add")) {
@@ -126,20 +122,34 @@ public class FarmProtect extends JavaPlugin implements Listener {
 					sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
 					return true;
 				}
-			} else {
-				sendHelp(sender);
+			} else if (args[0].equalsIgnoreCase("toggle")) {
+			    if (args[1].equalsIgnoreCase("multiworld")) {
+			        toggleConfig(sender, "MultiWorld", "farmprotect.multiworld.toggle");
+			    } else if (args[1].equalsIgnoreCase("explosionprotect")) {
+			        toggleConfig(sender, "ExplosionProtect", "farmprotect.explosionprotect.toggle");
+			    } else {
+			        sendHelp(sender, 2);
+			    }
+            } else {
+				sendHelp(sender, 1);
 			}
 		} 
 		return false;
 	}
 	
-	public void sendHelp(CommandSender sender) {
+	public void sendHelp(CommandSender sender, int page) {
 		sender.sendMessage(formatColor("&6o0=======&c[&eFarmProtect&c]&6========0o"));
 		sender.sendMessage(formatColor("&bNOTE &f: &e" + "All farmprotect commands have an alias of /fp"));
-		sender.sendMessage(formatColor("&b/fp toggle &f- &e" + "Turns on multiworld protection support"));
-    	sender.sendMessage(formatColor("&b/fp add <world> &f- &e" + "Adds a world into protection"));
-    	sender.sendMessage(formatColor("&b/fp remove <world> &f- &e" + "Removes protection for a world"));
-    	sender.sendMessage(formatColor("&b/fp reload &f- &e" + "Reloads the config"));
+		if (page == 2) {
+		    sender.sendMessage(formatColor("&b/fp toggle multiworld &f- &e" + "Turns on/off multiworld protection support"));
+		    sender.sendMessage(formatColor("&b/fp toggle explosionprotect &f- &e" + "Turns on/off explosion protection support"));
+		} else {
+		    //Default page
+		    sender.sendMessage(formatColor("&b/fp toggle <option> &f- &e" + "Toggles certain config values /fp toggle for help"));
+	        sender.sendMessage(formatColor("&b/fp add <world> &f- &e" + "Adds a world into protection"));
+	        sender.sendMessage(formatColor("&b/fp remove <world> &f- &e" + "Removes protection for a world"));
+	        sender.sendMessage(formatColor("&b/fp reload &f- &e" + "Reloads the config"));
+		}
 	}
 	
 	public String formatColor(String message) {
@@ -170,6 +180,63 @@ public class FarmProtect extends JavaPlugin implements Listener {
 				event.setCancelled(true);
 			}
 		}
+	}
+	
+	@EventHandler
+    public void onBlockExplode(BlockExplodeEvent event) {
+	    if (this.getConfig().getBoolean("ExplosionProtect")) {
+	        if (this.getConfig().getBoolean("MultiWorld")) {
+	            if (!worlds.contains(event.getBlock().getWorld().getName().toLowerCase())) {
+	                return;
+	            }
+	        }
+	        
+	        Iterator<Block> blockList = event.blockList().iterator();
+	        while (blockList.hasNext()) {
+	            Block block = blockList.next();
+	            if (block.getType() == Material.SOIL || cropsList.contains(block.getType())) {
+	                blockList.remove();
+	            }
+	        }
+	    }
+    }
+	
+	@EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+	    if (this.getConfig().getBoolean("ExplosionProtect")) {
+	        if (this.getConfig().getBoolean("MultiWorld")) {
+	            if (!worlds.contains(event.getEntity().getWorld().getName().toLowerCase())) {
+	                return;
+	            }
+	        }
+	        
+	        Iterator<Block> blockList = event.blockList().iterator();
+	        while (blockList.hasNext()) {
+	            Block block = blockList.next();
+	            if (block.getType() == Material.SOIL || cropsList.contains(block.getType())) {
+	                blockList.remove();
+	            }
+	        }
+	    }
+    }
+	
+	public void toggleConfig(CommandSender sender, String configKey, String permission) {
+	    if (sender.hasPermission(permission)) {
+            if (this.getConfig().getBoolean(configKey)) {
+                this.getConfig().set(configKey, false);
+                sender.sendMessage(chatPluginName + ChatColor.RED + configKey + " has been turned off.");
+            } else {
+                this.getConfig().set(configKey, true);
+                sender.sendMessage(chatPluginName + ChatColor.GREEN + configKey + " has been turned on.");
+            }
+            try {
+                this.getConfig().save(new File(getDataFolder(), "config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } 
+        } else {
+            sender.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
+        }
 	}
 	
 	private void createConfig() {
